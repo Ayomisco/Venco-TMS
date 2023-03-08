b0c5b6cf0d7593f3e2a6e8fe8ed70e52ce36e581
ayomisco <fayomide324@gmail.com>
2023-03-08 09:24:33 -0500

    Updated the readme

---
+++ tms/tms/doctype/agent/agent.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ayomisco and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Agent", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tms/tms/doctype/billing/test_billing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ayomisco and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBilling(FrappeTestCase):
+	pass
